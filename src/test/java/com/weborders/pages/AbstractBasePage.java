89be00bcd9b21cfd825c7f4b5882dea04d5afb71
skinox <jumaev@Aidars-MBP.verizon.net>
2020-04-05 16:46:29 -0400

    final commit of the Sunday

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -1,8 +1,12 @@
 package com.weborders.pages;
 
 import com.weborders.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
@@ -12,9 +16,28 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class AbstractBasePage {
 
     protected WebDriver driver = Driver.getDriver();
-    protected WebDriverWait wait = new WebDriverWait(driver,25);
+    protected WebDriverWait wait = new WebDriverWait(driver,20);
+
+    @FindBy(tagName = "h1")
+    protected WebElement pageLoad;
+
+    public String getPageLogoText(){
+        return pageLoad.getText();
+    }
+
 
     public AbstractBasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    /**
+     * Specify to which tab you want to navigate, tab name is a parameter in the method below
+     * @param component
+     */
+
+    public void navigateTo(String component){
+        String locator = "//a[text()=]'"+component+"']";
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
+    }
+
 }
